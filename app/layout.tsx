2020458a9b7051f1fb3ba20a30ebfcebf7f93c27
abhinav <abhinav07c@gmail.com>
2025-03-22 16:52:23 +0530

    feat:posthog webanalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { PostHogProvider } from '../components/PostHogProvider'
 
 export const metadata: Metadata = {
   title: 'VFind - Visual Search Engine',
@@ -11,15 +12,16 @@ export default function RootLayout({ children }:
   Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-
       <body>
-        <div className="relative">
-          {/* <script
-            crossOrigin="anonymous"
-            src="//unpkg.com/react-scan/dist/auto.global.js"
-          /> */}
-          <div className="relative z-10">{children}</div>
-        </div>
+        <PostHogProvider>
+          <div className="relative">
+            {/* <script
+              crossOrigin="anonymous"
+              src="//unpkg.com/react-scan/dist/auto.global.js"
+            /> */}
+            <div className="relative z-10">{children}</div>
+          </div>
+        </PostHogProvider>
       </body>
     </html>
   )
