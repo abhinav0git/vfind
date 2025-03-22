2680b4725a026f0f2336f2f5724b97dc122c4044
abhinav <abhinav07c@gmail.com>
2025-03-22 20:26:52 +0530

    feat: reverted last commit & posthog api env changes

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -7,13 +7,19 @@ import { usePathname, useSearchParams } from "next/navigation"
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: "/ingest",
-      ui_host: "https://us.posthog.com",
-      capture_pageview: false, // We capture pageviews manually
-      capture_pageleave: true, // Enable pageleave capture
-    })
-  }, [])
+    const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+
+    if (posthogKey) {
+      posthog.init(posthogKey, {
+        api_host: "/ingest",
+        ui_host: "https://us.posthog.com",
+        capture_pageview: false, // We capture pageviews manually
+        capture_pageleave: true, // Enable pageleave capture
+      });
+    } else {
+      console.warn("PostHog key is missing. Analytics will be disabled.");
+    }
+  }, []);
 
   return (
     <PHProvider client={posthog}>
