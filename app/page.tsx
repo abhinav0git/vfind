967942ec510389361a3df9f519ccd952b4e19efc
abhinav <abhinav07c@gmail.com>
2025-05-12 22:01:13 +0530

    feat: smooth scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 "use client"
+import { useEffect } from "react";
 
 import LandingPage from "../landing-page"
 
-export default function SyntheticV0PageForDeployment() {
+export default function Page() {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
   return <LandingPage />
 }
